d373a8f0a88a8a1c93aa80c5e146f92e381743e4
duvanpalomino <duvanpalomino@gmail.com>
2023-11-03 17:56:43 -0500

    Ajustando los Warning de colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,9 @@
 const colors = require('tailwindcss/colors');
+delete colors['lightBlue'];
+delete colors['warmGray'];
+delete colors['trueGray'];
+delete colors['coolGray'];
+delete colors['blueGray'];
 
 module.exports = {
   purge: ['./src/**/*{html,js}'],
