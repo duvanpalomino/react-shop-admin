63cd76ef1e1359923ef666175042882faa14e233
duvanpalomino <duvanpalomino@gmail.com>
2023-11-03 18:01:45 -0500

    revision del proyecto y dejandolo listo para seguir con el curso

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,13 +6,13 @@ module.exports = {
     es6: true,
   },
   extends: [
-    "eslint:recommended",
-    "plugin:jsx-a11y/recommended",
-    "plugin:prettier/recommended",
-    "next",
-    "next/core-web-vitals",
+    `'eslint:recommended',
+    'plugin:jsx-a11y/recommended',
+    'plugin:prettier/recommended',
+    'next',
+    'next/core-web-vitals'`,
   ],
   rules: {
-    semi: ["error", "always"],
+    semi: ['error', 'always'],
   },
 };
