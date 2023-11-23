6ac80c0fca87a6a7757a39f3e88ff8dd2d98fdf4
duvanpalomino <duvanpalomino@gmail.com>
2023-11-23 17:25:29 -0500

    Implementacion de nuestro componente Chart en el dashboard

--- src/pages/dashboard/index.js
+++ src/pages/dashboard/index.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import endPoints from '@services/api';
 import useFetch from '@hooks/useFetch';
 import Paginate from '@components/Paginate';
+import { Chart } from '@common/Chart';
 
 const PRODUCT_LIMIT = 60;
 
@@ -11,8 +12,23 @@ export default function Dashboard() {
   const products = useFetch(endPoints.products.getList(PRODUCT_LIMIT, offsetProducts), offsetProducts);
   const totalProducts = useFetch(endPoints.products.getList(0, 0)).length;
 
+  const categoryNames = products?.map((product) => product.category);
+  const categoryCount = categoryNames?.map((category) => category.name);
+  
+  const countOccurrences = (arr) => arr.reduce((prev, curr) => ((prev[curr] = ++prev[curr] || 1), prev), {});
+
+  const data = {
+    datasets: [{
+      label: 'Categories',
+      data: countOccurrences(categoryCount),
+      borderWidth: 2,
+      backgroundColor: ['#ffbb11', '#c0c0c0', '#50AF95', 'f3ba2f', '#2a71d0' ],
+    }]
+  }
+
   return (
     <>
+      < Chart className="mb-8 mt-2" chartData={data} />
       <div className="flex flex-col">
         <div className="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
           <div className="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
